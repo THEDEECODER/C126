a1966257a17ca346cbf0fb841d2d3c5d2be9b2ac
Anmol Aryan <87566476+THEDEECODER@users.noreply.github.com>
2022-04-11 19:06:08 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,11 +3,12 @@ leftwristy = 0;
 rightwristx = 0;
 rightwristy = 0;
 song = "";
+scoreLWY ="0";
 function preload(){
 song = loadSound("music1.mp3");
 }
 function setup(){
-    canvas = createCanvas(400, 500);
+    canvas = createCanvas(600, 500);
     canvas.center();
     video = createCapture(VIDEO);
     video.hide();
@@ -15,11 +16,20 @@ function setup(){
     posenet.on('pose', gotposes);
 }
 function draw() {
-    image(video, 0, 0, 400, 500);
+    image(video, 0, 0, 600, 500);
+    stroke("#E63946");
+    fill("#E63946");
+    circle(leftwristx, leftwristy, 20);
+    if(scoreLWY > 0.2){
+        LWY = Number(leftwristy);
+        removedecimal = Math.floor(LWY*2);
+        vol = removedecimal/1000;
+        console.log(vol);
+    }
 }
 function play() {
     song.play();
-    song.setVolume(0.9);
+    song.setVolume(vol);
     song.rate(1);
 }
 function modelloaded(){
@@ -34,5 +44,8 @@ function gotposes(results){
         rightwristy = results[0].pose.rightWrist.y;
         console.log("Left wrist x coordinates = "+leftwristx+ "Left wrist y coordinates = "+leftwristy);
         console.log("Right wrist x coordinates = "+rightwristx+ "Right wrist y coordinates = "+leftwristy);
+        scoreLWY = results[0].pose.keypoints[9].score;
+        console.log("Left Wrist score = "+scoreLWY);
     }
+    
 }
