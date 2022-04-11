afbd160b02a8dc2a9be3bcab4f251493414c7e4f
Anmol Aryan <87566476+THEDEECODER@users.noreply.github.com>
2022-04-11 19:03:44 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,18 +1,38 @@
-song = ""
+leftwristx = 0;
+leftwristy = 0;
+rightwristx = 0;
+rightwristy = 0;
+song = "";
 function preload(){
 song = loadSound("music1.mp3");
 }
-
 function setup(){
-    canvas = createCanvas(600, 500);
+    canvas = createCanvas(400, 500);
     canvas.center();
-    Video = createCapture(VIDEO);
-    Video.hide();
+    video = createCapture(VIDEO);
+    video.hide();
+    posenet = ml5.poseNet(video, modelloaded);
+    posenet.on('pose', gotposes);
 }
-
-function draw(){
-    image(Video, 0, 0, 600, 500);
+function draw() {
+    image(video, 0, 0, 400, 500);
 }
-function play(){
+function play() {
     song.play();
+    song.setVolume(0.9);
+    song.rate(1);
+}
+function modelloaded(){
+    console.log("posenet is initialized");
+}
+function gotposes(results){
+    if( results.length > 0 ){
+        console.log(results);
+        leftwristx = results[0].pose.leftWrist.x;
+        leftwristy = results[0].pose.leftWrist.y;
+        rightwristx = results[0].pose.rightWrist.x;
+        rightwristy = results[0].pose.rightWrist.y;
+        console.log("Left wrist x coordinates = "+leftwristx+ "Left wrist y coordinates = "+leftwristy);
+        console.log("Right wrist x coordinates = "+rightwristx+ "Right wrist y coordinates = "+leftwristy);
+    }
 }
